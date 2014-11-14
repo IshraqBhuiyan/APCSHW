da98a9c29a55d537064401f7772688eb427f9100
IshraqBhuiyan <ishraqabdulghaffar@gmail.com>
2014-11-14 09:18:38 -0500

    fixed ArrayListMethods

--- 02ArrayList/ArrayListMethods.java
+++ 02ArrayList/ArrayListMethods.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class ArrayListMethods{
     public static void collapseDuplicates(ArrayList<Integer> L){
 	for(int i=0; i<L.size(); i++){
-	    while(L.get(i+1) == L.get(i)){
+	    if(L.get(i+1) == L.get(i)){
 		L.remove(i+1);
 	    }
 	}
@@ -12,7 +12,7 @@ public class ArrayListMethods{
     public static void randomize(ArrayList<Integer> L){
 	for(int i=0;i<L.size(); i++){
 	    Random r = new Random();
-	    L.add(L.remove(L.get(r.nextInt(L.size()))));
+	    L.add(L.remove(r.nextInt(L.size())));
 	}
     }
 
@@ -22,7 +22,9 @@ public class ArrayListMethods{
 	    L1.add(i);
 	    L1.add(i);
 	}
+	System.out.println(L1);
 	collapseDuplicates(L1);
+	System.out.println(L1);
 	randomize(L1);
 	System.out.println(L1);
     }
