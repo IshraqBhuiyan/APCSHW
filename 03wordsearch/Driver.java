2bf8fb31e6a9a221077415d6d089f7fb343d950d
IshraqBhuiyan <ishraqbhuiyan@gmail.com>
2014-12-02 23:06:20 -0500

    another fix

--- 03wordsearch/Driver.java
+++ 03wordsearch/Driver.java
@@ -3,20 +3,21 @@ import java.io.*;
 public class Driver{
     public static void main(String[]args){
 	// parsing args
+	WordGrid w;
 	if(args.length == 0){
-	    WordGrid w = new WordGrid(20, 20, 0, true);
+	    w = new WordGrid(20, 20, 0, true);
 	}else if(args.length == 1){
 	    int rows = Integer.parseInt(args[0]);
-	    WordGrid w = new WordGrid(rows, 20, 0, true);
+	    w = new WordGrid(rows, 20, 0, true);
 	}else if(args.length == 2){
 	    int rows = Integer.parseInt(args[0]);
 	    int cols = Integer.parseInt(args[1]);
-	    WordGrid w = new WordGrid(rows, cols, 0, true);
+	    w = new WordGrid(rows, cols, 0, true);
 	}else if(args.length == 3){
 	    int rows = Integer.parseInt(args[0]);
 	    int cols = Integer.parseInt(args[1]);
 	    int seed = Integer.parseInt(args[2]);
-	    WordGrid w = new WordGrid(rows, cols, seed, true);
+	    w = new WordGrid(rows, cols, seed, true);
 	}else {
 	    int rows = Integer.parseInt(args[0]);
 	    int cols = Integer.parseInt(args[1]);
@@ -25,7 +26,7 @@ public class Driver{
 	    if(Integer.parseInt(args[3]) == 1){
 		filling = false;
 	    }
-	    WordGrid w = new WordGrid(rows, cols, seed, filling);
+	    w = new WordGrid(rows, cols, seed, filling);
 	}
 	w.loadWordsFromFile("words.txt");
 	System.out.println("Find these words:\n" + w.wordsInPuzzle());
