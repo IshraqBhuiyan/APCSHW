36d974377621245e86230feffaf4fffab5f409df
IshraqBhuiyan <ishraqbhuiyan@gmail.com>
2014-12-02 20:35:36 -0500

    some changes I had forgotten to actually ever add...

--- 03wordsearch/Driver.java
+++ 03wordsearch/Driver.java
@@ -1,17 +1,21 @@
 import java.util.*;
 import java.io.*;
 public class Driver{
-    public static  void openFile(String filename, ArrayList<String> wordList){
+    /*public static  void openFile(String filename, ArrayList<String> wordList){
 	File text = new File(filename);
 	Scanner words = new Scanner(text);
 	while(words.hasNextLine()){
 	    wordList.add(words.nextLine());
 	}
-    }
+	}*/
     public static void main(String[]args){
 	ArrayList<String> wordList = new ArrayList<String>();
-	String filename = "";
-	openFile(filename, wordList);
+	String filename = "words.txt";
+	//openFile(filename, wordList);
+	WordGrid test = new WordGrid(10, 10);
+	Random rng = new Random();
+	test.addWordHorizontal("bacon", 0, 0);
+	System.out.println(test);
 	
     }
 
