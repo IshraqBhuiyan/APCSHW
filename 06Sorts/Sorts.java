5801c1873674e7cb99eecfc998fb407469056b4b
IshraqBhuiyan <ishraqbhuiyan@gmail.com>
2014-12-22 02:49:37 -0500

    implement insertion and selection sort along with system time calculations for data collection

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -8,8 +8,30 @@ public class Sorts{
 	    c[i] = r.nextInt(100000);
 	}
 	System.out.println("unsorted list: \n" + Arrays.toString(c));
-	bubble(c);
-	System.out.println("sorted list: \n" + Arrays.toString(c));
+	long diff;
+	if(args.length > 0 && args[0].equals("bubble")){
+	    long start = System.currentTimeMillis();
+	    bubble(c);
+	    long end = System.currentTimeMillis();
+	    diff = end - start;
+	}
+	else if(args.length > 0 && args[0].equals("insertion")){
+	    long start = System.currentTimeMillis();
+	    insertion(c);
+	    long end = System.currentTimeMillis();
+	    diff = end - start;
+	}
+	else{
+	    long start = System.currentTimeMillis();
+	    selection(c);
+	    long end = System.currentTimeMillis();
+	    diff = end - start;
+	}
+	//bubble(c);
+	//insertion(c);
+	
+	//selection(c);
+	System.out.println("sorted list: \n" + Arrays.toString(c) + "\n" + "Time taken for sort: \n" + diff + " milliseconds\n");
     }
     public static void bubble(int[]c){
 	int length = c.length;
@@ -27,9 +49,28 @@ public class Sorts{
     }
     
     public static void insertion(int[]c){
+	for(int i=1;i<c.length;i++){
+	    int temp = c[i];
+	    int z = i-1;
+	    while(z>=0 && c[z] > temp){
+		c[z+1] = c[z];
+		z--;
+	    }
+	    c[z+1] = temp;
+	}
     }
 
     public static void selection(int[]c){
+	for(int i=0;i<c.length;i++){
+	    int least = i;
+	    for(int z=i;z<c.length;z++){
+		if(c[z]<c[least]) least = z;
+	    }
+	    int temp = c[i];
+	    c[i] = c[least];
+	    c[least] = temp;
+	}
+		
     }
 
 
